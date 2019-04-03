90a53416926312ac6ed2dcf07b9dddb9ceb4a8a6
Samuel Lange <samuelmanuel.lange@gmail.com>
2019-04-03 12:46:28 +0200

    routing still refuses to work

--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule
   ],
-  exports: [AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
